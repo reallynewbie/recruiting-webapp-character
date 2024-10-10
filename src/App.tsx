1d2571c3ce3003e09576c672bda05882eb1ad66b
reallynewbie <leung.bruce89@gmail.com>
2024-10-09 20:27:13 -0600

    Add Class List and show the class if requirements are met

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts";
 import { increaseAttribute, decreaseAttribute } from "./attributeHelpers";
 
 function App() {
-  const [str, setStr] = useState<number>(0);
-  const [dex, setDex] = useState<number>(0);
-  const [con, setCon] = useState<number>(0);
-  const [int, setInt] = useState<number>(0);
-  const [wis, setWis] = useState<number>(0);
-  const [chr, setChr] = useState<number>(0);
+  const [availableClasses, setAvailableClasses] = useState<string[]>([]);
+  const classes = CLASS_LIST
+
+  const statsMap = {
+    Strength: {
+      stat: str,
+      setStat: setStr,
+    },
+    Dexterity: {
+      stat: dex,
+      setStat: setDex,
+    },
+    Constitution: {
+      stat: con,
+      setStat: setCon,
+    },
+    Intelligence: {
+      stat: int,
+      setStat: setInt,
+    },
+    Wisdom: {
+      stat: wis,
+      setStat: setWis,
+    },
+    Charisma: {
+      stat: chr,
+      setStat: setChr,
+    },
+  };
+
+  useEffect(() => {
+    let approvedClasses = [];
+    Object.entries(CLASS_LIST).forEach(([className, classStats]) => {
+      // Using every so we skip the check if we miss even one stat
+      const statCheck = Object.entries(classStats).every(
+        ([attributeName, attributeValue]) => {
+          if (attributeName in statsMap) {
+            if (statsMap[attributeName]["stat"] >= attributeValue) {
+              return className;
+            } else {
+              return false;
+            }
+          } else {
+            console.log("new attribute maybe?");
+            return false;
+          }
+        }
+      );
+      if (statCheck) {
+        approvedClasses.push(className);
+      }
+    });
+    setAvailableClasses(approvedClasses);
+  }, [str, dex, con, int, wis, chr]);
 
   return (
     <div className="App">
@@ -47,6 +95,18 @@ function App() {
           <button onClick={() => increaseAttribute(chr, setChr)}>+</button>
           <button onClick={() => decreaseAttribute(chr, setChr)}>-</button>
         </div>
+      </section>
+      <section className="App-section">
+        Available Classes:
+        {Object.keys(CLASS_LIST).map((value) => (
+          <div>{value}</div>
+        ))}
+      </section>
+      <section className="App-section">
+        Meets Requirements For The Following Classes:
+        {availableClasses.map((value) => (
+          <div>{value}</div>
+        ))}
       </section>
     </div>
   );
